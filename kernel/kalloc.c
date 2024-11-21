0bdf99d847a31337358ea9527d945dc89e1dc613
Lori0221 <1183407244@qq.com>
2024-11-21 18:25:57 +0900

    完成cow lab，理解写时复制相关知识

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,13 +21,65 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
+  char* ref_page;
+  int page_cnt;
+  char *end_;
 } kmem;
+
+
+int
+pagecnt(void *pa_start, void *pa_end)
+{
+  char *p;
+  int cnt = 0;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    cnt++;
+  }
+  return cnt;
+}
 
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+
+  kmem.page_cnt = pagecnt(end, (void *) PHYSTOP);
+
+  kmem.ref_page = end;
+  for(int i = 0; i < kmem.page_cnt; i++){
+    kmem.ref_page[i] = 0;
+  }
+
+  kmem.end_ = kmem.ref_page + kmem.page_cnt;
+
+
+  freerange(kmem.end_, (void*)PHYSTOP);
+}
+
+int page_idx(uint64 pa){
+  pa = PGROUNDDOWN(pa);
+  int res = (pa - (uint64)end) / PGSIZE;
+  if(res < 0 || res >= kmem.page_cnt){
+    panic("page_idx illegal!");
+  }
+  return res;
+}
+
+void
+incr(void *pa){
+  int idx = page_idx((uint64)pa);
+  acquire(&kmem.lock);
+  kmem.ref_page[idx]++;
+  release(&kmem.lock);
+}
+
+void
+decr(void *pa){
+  int idx = page_idx((uint64)pa);
+  acquire(&kmem.lock);
+  kmem.ref_page[idx]--;
+  release(&kmem.lock);
 }
 
 void
@@ -35,8 +87,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,6 +100,14 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  int idx = page_idx((uint64)pa);
+  if(kmem.ref_page[idx] > 1){
+    decr(pa);
+    return;
+  }
+  if(kmem.ref_page[idx] == 1){
+    decr(pa);
+  }
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -72,11 +134,14 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+  }
   release(&kmem.lock);
 
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
+    incr(r);
+  }
   return (void*)r;
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -159,6 +159,7 @@ mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm)
     if(*pte & PTE_V)
       panic("remap");
     *pte = PA2PTE(pa) | perm | PTE_V;
+
     if(a == last)
       break;
     a += PGSIZE;
@@ -307,11 +308,12 @@ uvmfree(pagetable_t pagetable, uint64 sz)
 // frees any allocated pages on failure.
 int
 uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
-{
+{ 
+  // 原本是直接复制的
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+  // char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -319,15 +321,25 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+
+    // 清除PTE的写权限
+    *pte = *pte & ~(PTE_W);
+    *pte = *pte | PTE_COW;
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+
+
+    // if((mem = kalloc()) == 0)
+    //   goto err;
+    // memmove(mem, (char*)pa, PGSIZE);
+    // if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
+      // 相同映射
+      // kfree(mem);
       goto err;
     }
+    incr((void *) pa);
   }
+  
   return 0;
 
  err:
@@ -358,6 +370,13 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+
+    if(is_cow_fault(pagetable, va0)){
+      if(cow_alloc(pagetable, va0) < 0){
+        return -1;
+      }
+    }
+
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
@@ -439,4 +458,55 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+int
+is_cow_fault(pagetable_t pagetable, uint64 va)
+{
+  if(va >= MAXVA){
+    return 0;
+  }
+  va = PGROUNDDOWN(va);
+
+  pte_t *pte = walk(pagetable, va, 0);
+  if(pte == 0)
+    return 0;
+  if((*pte & PTE_V) == 0)
+    return 0;
+  if((*pte & PTE_U) == 0)
+    return 0;
+  
+  if(*pte & PTE_COW){
+    return 1;
+  }
+
+  return 0;
+}
+
+int
+cow_alloc(pagetable_t pagetable, uint64 va)
+{
+  va = PGROUNDDOWN(va);
+  pte_t *pte = walk(pagetable, va, 0);
+
+  uint64 pa = PTE2PA(*pte);
+  int flag = PTE_FLAGS(*pte);
+
+  char *mem = kalloc();
+  if(mem == 0){
+    return -1;
+  }
+  memmove(mem, (char *)pa, PGSIZE);
+  // 解除映射，再重新映射
+  uvmunmap(pagetable, va, 1, 1);
+
+  flag &= ~(PTE_COW);
+  flag |= PTE_W;
+
+  if(mappages(pagetable, va, PGSIZE, (uint64) mem, flag) < 0){
+    kfree(mem);
+    return -1;
+  }
+
+  return 0;
 }
